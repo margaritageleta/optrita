809d6de3b351756a50b4c7f11c4e38d964712b50
3omni <noticiasmundiales99@gmail.com>
2019-11-01 21:49:21 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+requires = ['numpy', 'matplotlib.pyplot', 'pandas', 'scipy', 'sympy']
+
+package_dir = {'optrita' : 'optrita'}
+package_data = {'optrita : []}
+
+setup(
+	name = 'optrita',
+	version = '1.0.0',
+	license = '.',
+	author = '3omni',
+	author_email = 'rita.geleta@jediupc.com',
+	url = 'https://github.com/margaritageleta/optrita',
+	keywords = ['python', 'optimization'],
+	package_data = package_data,
+	package_dir = package_dir
+)
