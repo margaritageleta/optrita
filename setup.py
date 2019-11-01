0f14b9ac13f0306324e6c55c24c853ff836b4155
3omni <31892476+margaritageleta@users.noreply.github.com>
2019-11-01 23:55:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,15 +12,30 @@ package_data = {'optrita' : []}
 
 setup(
 	name = 'optrita',
-	version = '1.0.0',
-	license = '.',
+	packages = packages,
+	version = '0.0.1',
+	license = 'MIT',
+	description = 'Optimization and Machine Learning from 0'
 	author = '3omni',
 	author_email = 'rita.geleta@jediupc.com',
 	url = 'https://github.com/margaritageleta/optrita',
+	download_url = 'https://github.com/margaritageleta/optrita/archive/0.0.1.tar.gz',
 	keywords = ['python', 'optimization'],
+	install_requires = requires,
+	classifiers=[
+    		'Development Status :: 3 - Alpha',     
+    		'Intended Audience :: Developers',     
+    		'Topic :: Software Development :: Build Tools',
+    		'License :: OSI Approved :: MIT License',   
+    		'Programming Language :: Python :: 3',     
+    		'Programming Language :: Python :: 3.4',
+    		'Programming Language :: Python :: 3.5',
+    		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+  	],
 	package_data = package_data,
-	package_dir = package_dir
+	package_dir = package_dir,
 )
 
 __author__ = {'3omni' : 'rita.geleta@jediupc.com'}
-__version__ = '1.0.0'
+__version__ = '0.0.1'
